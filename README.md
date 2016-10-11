576c6a8c57a3b7d1031806c8832ed56ee02fe4a1
Matteo Collina <hello@matteocollina.com>
2016-10-11 12:43:17 +0200

    Added application code.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # slow-rest-api
-A REST API that is slow
+
+A REST API that is slow.
+
+Part of the workshop __Optimizing Node.js applications with 0x and autocannon__
+
+## Steps
+
+1. clone this repository
+2. `npm install`
+3. `node index`
+4. In another terminal: `npm test`
+
+## Generating a flamegraph
+
+1. Make sure you are on Linux or Mac OS X, and you have superuser
+   permissions
+2. Install [0x](http://npm.im/0x) with `npm install 0x -g`
+3. `0x index`
+4. In another terminal, as soon as possible: `npm test`
+5. CTRL-C the `0x` process.
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,56 @@
+'use strict'
+
+var etag = require('./etag')
+var pkg = JSON.stringify(require('./package.json'))
+
+var restify = require('restify')
+var server = restify.createServer()
+var count = 1
+
+server.get('/a', function (req, res, next) {
+  var tag = etag(pkg + ++count)
+
+  if (!(tag instanceof Error)) {
+    res.setHeader('ETag', tag)
+  }
+
+  res.send(pkg)
+  return next()
+})
+
+server.get('/b', function (req, res, next) {
+  var tag = etag({entity: pkg + ++count, algorithm: 'sha256'})
+
+  if (!(tag instanceof Error)) {
+    res.setHeader('ETag', tag)
+  }
+
+  res.send(pkg)
+  return next()
+})
+
+server.get('/c', function (req, res, next) {
+  var tag = etag(pkg + ++count, {algorithm: 'sha512WithRsaEncryption'})
+
+  if (!(tag instanceof Error)) {
+    res.setHeader('ETag', tag)
+  }
+
+  res.send(pkg)
+  return next()
+})
+
+server.listen(3000)
+
+var signal = 'SIGINT'
+
+// Cleanly shut down process on SIGINT to ensure that perf-<pid>.map gets flushed
+process.on(signal, onSignal)
+
+function onSignal () {
+  console.error('count', count)
+  // IMPORTANT to log on stderr, to not clutter stdout which is purely for data, i.e. dtrace stacks
+  console.error('Caught', signal, ', shutting down.')
+  server.close()
+  process.exit(0)
+}
