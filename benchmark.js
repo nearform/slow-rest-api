cccc71f14a89d99f6fe0680e8d295e4eeaf1e084
Mathias Buus <mathiasbuus@gmail.com>
2018-10-31 15:51:13 +0100

    comments and some whitespace

--- benchmark.js
+++ benchmark.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const os = require('os')
 const autocannon = require('autocannon')
 const { PassThrough } = require('stream')
 
@@ -17,11 +18,17 @@ function run (url) {
 
   outputStream.on('data', data => buf.push(data))
   inst.on('done', function () {
+    process.stdout.write(os.EOL)
     process.stdout.write(Buffer.concat(buf))
   })
 }
 
 console.log('Running all benchmarks in parallel ...')
+
+// We use multiple autocannon instances so we get
+// output for each of them to see which one is faster.
+// If we are just profiling the server as a whole a single
+// instance could be used instead.
 
 run('http://localhost:3000/a')
 run('http://localhost:3000/b')
