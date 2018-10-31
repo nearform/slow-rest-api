fd837de9636253cf547a409bf4065692d6983d3e
Mathias Buus <mathiasbuus@gmail.com>
2018-10-31 13:48:17 +0100

    add js benchmark runner for easy cross platform

---
+++ benchmark.js
@@ -0,0 +1,27 @@
+const autocannon = require('autocannon')
+const { PassThrough } = require('stream')
+
+function run (url) {
+  const buf = []
+  const outputStream = new PassThrough()
+
+  const inst = autocannon({
+    url,
+    connections: 100,
+    duration: 20
+  })
+
+  autocannon.track(inst, { outputStream })
+
+  outputStream.on('data', data => buf.push(data))
+  inst.on('done', function () {
+    process.stdout.write(Buffer.concat(buf))
+  })
+}
+
+console.log('Running all benchmarks in parallel ...')
+
+run('http://localhost:3000/a')
+run('http://localhost:3000/b')
+run('http://localhost:3000/c')
+
